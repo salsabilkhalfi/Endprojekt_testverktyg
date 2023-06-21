ff478fe62c77b4ba20363021c113cfe4c57cd93b
Viktor Svensson <>
2023-06-21 07:19:27 +0200

    init commit

--- src/main/java/com/example/spring_thymeleaf/SpringThymeleafApplication.java
+++ src/main/java/com/example/spring_thymeleaf/SpringThymeleafApplication.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @SpringBootApplication
 public class SpringThymeleafApplication{
+
     public static void main(String[] args) {
         SpringApplication.run(SpringThymeleafApplication.class, args);
     }
