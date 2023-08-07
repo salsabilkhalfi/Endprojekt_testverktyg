1253411f2c6967238226be02a56cb0c60e3b1ef7
Viktor Svensson <10098648-viktorsvensson@users.noreply.gitlab.com>
2023-08-07 10:18:20 +0200

    init commit

--- src/main/java/com/example/spring_thymeleaf/controller/LapTimeController.java
+++ src/main/java/com/example/spring_thymeleaf/controller/LapTimeController.java
@@ -27,34 +27,6 @@ public class LapTimeController {
     public String findAll(Model model){
         List<LapTime> lapTimeList = lapTimeService.findLapTimes();
         model.addAttribute("lapTimeList", lapTimeList);
-
-
-        int i =0;
-        int num =0;
-
-        //Empty String
-        String  primeNumbers = "";
-
-        for (i = 1; i <= 50000; i++)
-        {
-            int counter=0;
-            for(num =i; num>=1; num--)
-            {
-                if(i%num==0)
-                {
-                    counter = counter + 1;
-                }
-            }
-            if (counter ==2)
-            {
-                //Appended the Prime number to the String
-                primeNumbers = primeNumbers + i + " ";
-            }
-        }
-        System.out.println("Prime numbers from 1 to 100 are :");
-        System.out.println(primeNumbers);
-
-
         return "laptime";
     }
 
