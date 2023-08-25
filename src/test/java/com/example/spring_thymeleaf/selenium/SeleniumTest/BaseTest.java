42050d5e285d7357aaa9a74facc4c32814378599
salsabilkhalfi <salsabil.khalfi@gmail.com>
2023-08-25 12:49:27 +0200

    End commit

--- src/test/java/com/example/spring_thymeleaf/selenium/SeleniumTest/BaseTest.java
+++ src/test/java/com/example/spring_thymeleaf/selenium/SeleniumTest/BaseTest.java
@@ -1,14 +1,9 @@
 package com.example.spring_thymeleaf.selenium.SeleniumTest;
 
-import com.example.spring_thymeleaf.selenium.Pages.LandingPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.safari.SafariDriver;
 
@@ -16,7 +11,7 @@ import org.openqa.selenium.safari.SafariDriver;
 public class BaseTest {
     public WebDriver driver;
     private static String baseURL = "http://localhost:8080/laptimes";
-    public LandingPage landingPage;
+
 
     @BeforeAll
     public static void init(){
@@ -32,8 +27,7 @@ public class BaseTest {
         driver = new SafariDriver();
         driver.manage().window().maximize();
         driver.get(baseURL);
-        landingPage = new LandingPage();
-        landingPage.setDriver(driver);
+
     }
 
    // @AfterEach
--- src/test/java/com/example/spring_thymeleaf/service/LapTimeServiceTest.java
+++ src/test/java/com/example/spring_thymeleaf/service/LapTimeServiceTest.java
@@ -2,6 +2,7 @@ package com.example.spring_thymeleaf.service;
 
 import com.example.spring_thymeleaf.entities.LapTime;
 import com.example.spring_thymeleaf.repo.LapTimeRepo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -12,42 +13,46 @@ import static org.mockito.Mockito.*;
 
 class LapTimeServiceTest {
 
-    LapTimeRepo lapTimeRepo = mock(LapTimeRepo.class);
-    List<LapTime> allLapTimes = Arrays.asList(
-            new LapTime(1),
-            new LapTime(15),
-            new LapTime(2),
-            new LapTime(16),
-            new LapTime(3),
-            new LapTime(5.5),
-            new LapTime(7),
-            new LapTime(13),
-            new LapTime(5),
-            new LapTime(7)
-    );
 
+
+    private LapTimeRepo lapTimeRepo;
+    private List<LapTime> allLapTimes;
+
+    @BeforeEach
+    public void setup() {
+        lapTimeRepo = mock(LapTimeRepo.class);
+        allLapTimes = Arrays.asList(
+                new LapTime(1),
+                new LapTime(15),
+                new LapTime(2),
+                new LapTime(16),
+                new LapTime(3),
+                new LapTime(5.5),
+                new LapTime(7),
+                new LapTime(13),
+                new LapTime(5),
+                new LapTime(7)
+        );
+    }
 
     @Test
-    public void testSortLapTimesInDescendingOrder() {
-
+    public void testLapTimesInDescendingOrder() {
         when(lapTimeRepo.findAll()).thenReturn(allLapTimes);
+
         LapTimeService lapTimeService = new LapTimeService(lapTimeRepo);
-
-        System.out.println("All Lap Times:");
-        for (LapTime lapTime : allLapTimes) {
-            System.out.println(lapTime.getLapTime());
-        }
-
         List<LapTime> topLapTimes = lapTimeService.findLapTimes();
 
+        System.out.println("All Lap Times:");
+        allLapTimes.forEach(lapTime -> System.out.println(lapTime.getLapTime()));
+
         System.out.println("Top 5 Lap Times:");
-        for (LapTime lapTime : topLapTimes) {
-            System.out.println(lapTime.getLapTime());
-        }
+        topLapTimes.forEach(lapTime -> System.out.println(lapTime.getLapTime()));
 
         assertEquals(5, topLapTimes.size());
         for (int i = 1; i < topLapTimes.size(); i++) {
             assertTrue(topLapTimes.get(i - 1).getLapTime() <= topLapTimes.get(i).getLapTime());
+
+
         }
     }
 }
