4cc5d3c9afdfdb45f9bfd3a773474d815d6bcab1
salsabilkhalfi <56250361+salsabilkhalfi@users.noreply.github.com>
2023-08-25 02:36:04 +0200

    Update LandingPage.java

--- src/test/java/com/example/spring_thymeleaf/selenium/Pages/LandingPage.java
+++ src/test/java/com/example/spring_thymeleaf/selenium/Pages/LandingPage.java
@@ -1,14 +1,1 @@
-package com.example.spring_thymeleaf.selenium.Pages;
-
-import org.openqa.selenium.By;
-
-public class LandingPage extends BasePage {
-  private final  By addNewAddNewLapTime = By.id("addtodo-button");
-
-
-    public AddNewLapTime addNewLapTimeButton(){
-        click(addNewAddNewLapTime);
-        return new AddNewLapTime();
-    }
-}
 
